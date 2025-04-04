013db52b52c21d5999f0da965e5e0c5bcf3d7303
Bousef <osman.yousef6933@gmail.com>
2025-04-04 14:49:25 -0400

    changed database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,23 @@
-import { type Config } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+import { parse } from "pg-connection-string";
 
-import { env } from "~/env";
+dotenv.config();
+
+const config = parse(process.env.DATABASE_URL!);
 
 export default {
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    host: config.host!,
+    port: parseInt(config.port!),
+    user: config.user!,
+    password: config.password!,
+    database: config.database!,
+    ssl: {
+      rejectUnauthorized: false, // Required for Supabase connections
+    },
   },
   tablesFilter: ["algoshpe_*"],
 } satisfies Config;
